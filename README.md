10c852a7088c38fe601f945d32fd2d9f85f9c346
Ted Monchamp <ted.monchamp@gmail.com>
2018-11-13 23:19:38 -0500

    Fixed readme and output naming and format

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ﻿# Token Generator
 
-A highly opinionated D&D token generator that assumes your template uses #FF00FF for masking.
+A highly opinionated D&D token generator that assumes your template uses #FF00FF for masking.  Transparency is allowed, and the output is a PNG.  You can tell it whatever you want, but it'll be a PNG underneath.
 
 ## Usage
 
@@ -12,7 +12,7 @@ This doesn't include anything prebuilt, so you'll need the .NET Core 2 SDK.
 git clone git@github.com:thebargaintenor/token-generator
 cd token-generator
 dotnet build
-dotnet run
+dotnet run templatePath contentPath outputPath
 ```
 
 Or you could clone the repo and use MonoDevelop/VS for Mac.  The Nuget dependencies should factor in somewhere.
--- TokenGenerator/Program.fs
+++ TokenGenerator/Program.fs
@@ -1,4 +1,5 @@
 ﻿open System.Drawing
+open System.Drawing.Imaging
 
 open ImageOperations
 
@@ -15,21 +16,21 @@ let createToken (template: Bitmap) (image: Bitmap) (outputPath: string): string 
     match maskBounds with
     | Some bounds ->
         use token = applyMask template mask bounds image
-        token.Save(outputPath)
+        token.Save(outputPath, ImageFormat.Png)
         Success outputPath
     | None -> Failure "no fuchsia mask found"
 
 [<EntryPoint>]
 let main argv =
     match argv with
-    | [| templatePath ; imagePath ; _ |] -> 
+    | [| templatePath ; imagePath ; outputPath |] -> 
         printfn "template: %s, image: %s" templatePath imagePath
         try
             use template = loadbitmap templatePath
             use image = loadbitmap imagePath
-            let boundingBox = createToken template image "output.png"
+            let boundingBox = createToken template image outputPath
             match boundingBox with
-            | Success bounds -> printfn "Bounding box: %s" bounds
+            | Success outputLocation -> printfn "Success! Your token was written to %s" outputLocation
             | Failure message -> printfn "Error: %s" message
         with
             | _ -> printfn "failed to load image"
