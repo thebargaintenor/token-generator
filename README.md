abb98cfde5a811dd245cff31774d0e4885d83447
Ted Monchamp <ted.monchamp@gmail.com>
2020-12-13 10:55:47 -0500

    now with more CLI args than ever before

--- README.md
+++ README.md
@@ -1,22 +1,42 @@
 ﻿# Token Generator
 
-A highly opinionated D&D token generator that assumes your template uses #FF00FF for masking.  Transparency is allowed, and the output is a PNG.  You can tell it whatever you want, but it'll be a PNG underneath.
+A ~highly~ mostly opinionated D&D token generator.  Transparency is allowed, and the output is a PNG.  You can tell it whatever you want, but it'll be a PNG underneath.
+
+By default, it will assume your masking color is magenta (#FFOOFF), but that is configurable with `--mask`.  Valid RGB hex values are accepted, but if you use `#`, remember to quote your value to avoid confusing your shell.  
 
 ## Usage
 
-This doesn't include anything prebuilt, so you'll need the .NET Core 2 SDK.
+```
+USAGE: TokenGenerator.exe [--help] --template <template> --output <output> [--mask <Mask>] <image>
 
-### Got Linux or MacOS?
+IMAGE:
+
+    <image>               Path of image to apply to token template
+
+OPTIONS:
+
+    --template, -t <template>
+                          Path of token template
+    --output, -o <output> Output path for finished token
+    --mask, -m <mask>     Masking color as RGB hex value (default FF00FF)
+    --help                display this list of options.
+```
+
+### Building
+
+This doesn't include anything prebuilt, so you'll need the .NET Core 3.1 SDK.
+
+For all your Linux, MacOS, and WSL needs:
 
 ```
-git clone git@github.com:thebargaintenor/token-generator
-cd token-generator
+git clone git@github.com:thebargaintenor/token-generator.git
+cd token-generator/TokenGenerator
 dotnet build
-dotnet run templatePath contentPath outputPath
+dotnet run -t templatePath -o outputPath imagePath
 ```
 
-Or you could clone the repo and use MonoDevelop/VS for Mac.  The Nuget dependencies should factor in somewhere.
+You are welcome to use your IDE of choice if you'd prefer.
 
-### Windows?
+## Eventual Features
 
-Visual Studio is fine too.
+Processing in batches is on the to-do list, and I'll get there someday.
--- TokenGenerator/Program.fs
+++ TokenGenerator/Program.fs
@@ -1,13 +1,35 @@
-﻿open SixLabors.ImageSharp
+﻿open Argu
+open SixLabors.ImageSharp
 open SixLabors.ImageSharp.PixelFormats
 
 open ImageOperations
 
+type CliArgs =
+    | [<AltCommandLine("-t"); Mandatory; Unique>] Template of template: string
+    | [<AltCommandLine("-o"); Mandatory; Unique>] Output of output: string
+    | [<AltCommandLine("-m"); Unique>] Mask of mask: string
+    | [<MainCommand; ExactlyOnce; Last>] Image of image: string
+
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Template _ -> "Path of token template"
+            | Output _ -> "Output path for finished token"
+            | Mask _ -> "Masking color as RGB hex value (default FF00FF)"
+            | Image _ -> "Path of image to apply to token template"
+
+module Constraints =
+    let parseColor (rgbhex: string): Argb32 =
+        let mutable parsed = Color.Magenta;
+        if Color.TryParseHex(rgbhex, ref parsed) then
+            parsed.ToPixel<Argb32>()
+        else
+            failwith "Invalid color value provided"
+
 let loadbitmap (path: string) = Image.Load<Argb32>(path)
 
-let createToken (template: Image<Argb32>) (image: Image<Argb32>) (outputPath: string) = 
-    let magenta = Argb32(byte 255, byte 0, byte 255, byte 255)
-    findMaskBounds template magenta
+let createToken (maskingColor: Argb32) (template: Image<Argb32>) (image: Image<Argb32>) (outputPath: string) = 
+    findMaskBounds template maskingColor
     |> function
         | Some mask ->
             use token = applyMask template mask image
@@ -17,19 +39,35 @@ let createToken (template: Image<Argb32>) (image: Image<Argb32>) (outputPath: st
 
 [<EntryPoint>]
 let main argv =
-    match argv with
-    | [| templatePath ; imagePath ; outputPath |] -> 
-        try
-            use template = loadbitmap templatePath
-            use image = loadbitmap imagePath
-            createToken template image outputPath
-            |> function
-                | Ok outputLocation -> printfn "Success! Your token was written to %s" outputLocation
-                | Error message -> printfn "Error: %s" message
-        with
-            | ex -> printfn "something failed: %s \n %s" ex.Message ex.StackTrace
-    | _ ->
-        printfn "Missing arguments"
+    let errorHandler = 
+        ProcessExiter(
+            colorizer =
+                function
+                | ErrorCode.HelpText -> None
+                | _ -> Some System.ConsoleColor.Red)
+    
+    let parser = ArgumentParser.Create<CliArgs>(programName="TokenGenerator.exe", errorHandler=errorHandler)
+    let args = parser.ParseCommandLine argv
+
+    let templatePath = args.GetResult Template
+    let imagePath = args.GetResult Image
+    let outputPath = args.GetResult Output
+
+    let mask =
+        if args.Contains Mask then
+            args.PostProcessResult(<@ Mask @>, Constraints.parseColor)
+        else
+            Argb32(255.0f, 0.0f, 255.0f, 255.0f)
+
+    try
+        use template = loadbitmap templatePath
+        use image = loadbitmap imagePath
+        createToken mask template image outputPath
+        |> function
+            | Ok outputLocation -> printfn "Success! Your token was written to %s" outputLocation
+            | Error message -> printfn "Error: %s" message
+    with
+        | ex -> printfn "something failed: %s \n %s" ex.Message ex.StackTrace
 
     printfn "Exiting happily."
     0
